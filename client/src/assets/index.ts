54b2e23ab3e3505822ef099f4000c048d2f692b2
damattag <guilherme.damatta@yahoo.com.br>
2022-12-07 15:13:40 -0300

    created header

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,10 +1,17 @@
 import LogoCITi from './logo.png';
 import CITi from './CITi.png';
 import RightArrow from './rightarrow.png';
+import Overlay from './overlay.png';
+import HeaderImage from './headerImage.png';
+import ScrollDown from './scrollDown.png';
+
 
 
 export {
     LogoCITi,
     CITi,
-    RightArrow
+    RightArrow,
+    Overlay,
+    HeaderImage,
+    ScrollDown
 };
---
+++ client/src/pages/services/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, HeaderContainer, MeetUs, OverlayImage, ScrollImage, ServicesContainer, Text1, Text2, Text3 } from './styles';
+import { Overlay, ScrollDown } from '../../assets';
+
+export const Services: React.FC = () => {
+    return (
+        <Container>
+            <HeaderContainer>
+                <Text3>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </Text3>
+                <ScrollImage src={ScrollDown} alt=""/>
+            </HeaderContainer>
+            <ServicesContainer>
+                <Text1>
+                    <MeetUs>
+                        Conheça a Durbeton!
+                    </MeetUs>
+                    <Text2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </Text2>
+                </Text1>
+                <OverlayImage src= {Overlay} alt=""/>
+            </ServicesContainer>
+        </Container>
+    );
+}
