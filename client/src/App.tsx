3fc88da11d6045d477cf3871129895844690a908
Mateus Riff <mateusnriff@gmail.com>
2022-12-09 11:29:30 -0300

    Restored "return()" in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,18 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
 
-
 import { Home } from './pages';
 import { Navbar } from './pages';
 import { Services } from './pages';
 import { Footer } from './pages/footer';
 import { Form } from './pages/form';
+
 function App() {
   return (
-    <html>
-      <Form/>
-      
-    </html>
+    <ThemeProvider theme={theme}>
+      <Home/>
+      <GlobalStyle/>
+    </ThemeProvider>
   );
 }
 
